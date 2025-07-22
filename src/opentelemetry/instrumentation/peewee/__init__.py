e0c71577319032c8c03e7a8b2edbaa349855ce17
Warren Angelo H. Layson <warrenlayson@gmail.com>
2025-07-23 07:22:30 +0800

    feat(peewee): improve SQL commenting, metrics, and tracing
    - Enhanced SQL commenting with a new `_get_commenter_data` function.
    - Introduced attribute-based SQL commenter with `enable_attribute_commenter` option.
    - Updated tracing logic to support stricter adherence to OTEL specs.
    - Improved metrics with updated connection usage naming and attributes.
    - Updated dependencies to version 0.54b1.
    - Dropped Python 3.7 support in `pyproject.toml`.

--- src/opentelemetry/instrumentation/peewee/__init__.py
+++ src/opentelemetry/instrumentation/peewee/__init__.py
@@ -1,19 +1,22 @@
+import re
+from functools import wraps
 from timeit import default_timer
 from typing import Collection
-from functools import wraps
 
 import peewee
-from peewee import SENTINEL
-
 from opentelemetry import trace, metrics
-from opentelemetry.trace import SpanKind, Status, StatusCode
-from opentelemetry.semconv.trace import SpanAttributes
-from opentelemetry.instrumentation.sqlcommenter_utils import _add_sql_comment
-from opentelemetry.instrumentation.utils import _get_opentelemetry_values
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
 from opentelemetry.instrumentation.peewee.package import _instruments
 from opentelemetry.instrumentation.peewee.version import __version__
+from opentelemetry.instrumentation.sqlcommenter_utils import _add_sql_comment
+from opentelemetry.instrumentation.utils import _get_opentelemetry_values
+from opentelemetry.semconv.metrics import MetricInstruments
+from opentelemetry.semconv.trace import SpanAttributes
+from opentelemetry.trace import SpanKind, Status, StatusCode
+from peewee import SENTINEL
+from opentelemetry.util._importlib_metadata import version
 
+peewee_version = version("peewee")
 
 def _get_tracer(tracer_provider=None):
     return trace.get_tracer(
@@ -52,7 +55,15 @@ def _get_attributes_from_connect_params(params):
 def _get_operation_name(vendor, db_name, sql):
     parts = []
     if isinstance(sql, str):
-        parts.append(sql.split()[0])
+        # otel spec recommends against parsing SQL queries. We are not trying to parse SQL
+        # but simply truncating the statement to the first word. This covers probably >95%
+        # use cases and uses the SQL statement in span name correctly as per the spec.
+        # For some very special cases it might not record the correct statement if the SQL
+        # dialect is too weird but in any case it shouldn't break anything.
+        # Strip leading comments so we get the operation name.
+        parts.append(
+            re.compile(r'^/\*.*?\*/').sub("", sql).split()[0]
+        )
     if db_name:
         parts.append(db_name)
     if not parts:
@@ -72,12 +83,37 @@ def _normalize_vendor(vendor):
 
     raise ValueError
 
+def _get_commenter_data(self, commenter_options) -> dict:
+    """Calculate sqlcomment contents from conn and configured options"""
+    commenter_data = dict(
+        db_driver=self.__class__.__name__,
+        db_framework=f'peewee:{peewee_version}'
+    )
+
+    if commenter_options.get('opentelemetry_values', True):
+        commenter_data.update(**_get_opentelemetry_values())
+
+    # Filter down to just the requested attributes
+    commenter_data = {
+        k: v
+        for k, v in commenter_data.items()
+        if commenter_options.get(k, True)
+    }
+    return commenter_data
+
+def _set_db_client_span_attributes(vendor, span, sql, attrs) -> None:
+    """Uses sql and attrs to set attributes of provided Otel span"""
+    span.set_attribute(SpanAttributes.DB_STATEMENT, sql)
+    span.set_attribute(SpanAttributes.DB_SYSTEM, vendor)
+    for key, value in attrs.items():
+        span.set_attribute(key, value)
 
 def _wrap_execute_sql(
         duration_histogram,
         tracer=None,
         enable_sqlcommenter=False,
-        commenter_options=None
+        commenter_options=None,
+        enable_attribute_commenter=False,
 ):
     original_execute = peewee.Database.execute_sql
 
@@ -108,21 +144,25 @@ def _wrap_execute_sql(
                 for key, value in attrs.items():
                     span.set_attribute(key, value)
             if enable_sqlcommenter:
-                commenter_data = dict(
-                    db_driver=self.__class__.__name__,
-                    db_framework=f'peewee:{__version__}'
-                )
+                commenter_data = _get_commenter_data(self, commenter_options)
 
-                if commenter_options.get('opentelemetry_values', True):
-                    commenter_data.update(**_get_opentelemetry_values())
+                if enable_attribute_commenter:
+                    # just to handle type safety
+                    sql = str(sql)
 
-                # Filter down to just the requested attributes
-                commenter_data = {
-                    k: v
-                    for k, v in commenter_data.items()
-                    if commenter_options.get(k, True)
-                }
-                sql = _add_sql_comment(sql, **commenter_data)
+                    # sqlcomment is added to executed query and db.statement span attribute
+                    sql = _add_sql_comment(
+                        sql, **commenter_data
+                    )
+
+                    _set_db_client_span_attributes(vendor, span, sql, attrs)
+                else:
+                    # sqlcomment is only added to executed query
+                    # so db.statement is set before add_sql_comment
+                    _set_db_client_span_attributes(vendor, span, sql, attrs)
+                    sql = _add_sql_comment(
+                        sql, **commenter_data
+                    )
             try:
                 if span.is_recording():
                     span.set_status(
@@ -168,13 +208,13 @@ def _wrap_connect(tracer, active_connections):
                 _add_used_to_connection_usage(self, active_connections, 1)
                 return result
             except Exception as exc:
-                    span.record_exception(exc)
-                    span.set_status(
-                        Status(
-                            StatusCode.ERROR,
-                            str(exc)
-                        )
+                span.record_exception(exc)
+                span.set_status(
+                    Status(
+                        StatusCode.ERROR,
+                        str(exc)
                     )
+                )
                 raise
         return None
 
@@ -192,8 +232,9 @@ def _wrap_close(active_connections):
 
     return close
 
+
 # Can't use this as peewee can't hook into checkin/checkout
-def _add_idle_to_connection_usage(database,active_connections, value):
+def _add_idle_to_connection_usage(database, active_connections, value):
     active_connections.add(
         value,
         attributes={
@@ -201,6 +242,7 @@ def _add_idle_to_connection_usage(database,active_connections, value):
             "state": "idle"
         }
     )
+
 
 def _add_used_to_connection_usage(database, active_connections, value):
     active_connections.add(
@@ -210,6 +252,7 @@ def _add_used_to_connection_usage(database, active_connections, value):
             "state": "used"
         }
     )
+
 
 def _get_connection_string(database):
     if hasattr(database, 'connect_params'):
@@ -233,12 +276,15 @@ def _get_connection_string(database):
 
     return ''
 
+
 def _get_attributes_from_database(database):
     attrs = {}
 
     attrs["pool.name"] = _get_connection_string(database)
 
     return attrs
+
+
 class PeeweeInstrumentor(BaseInstrumentor):
     def instrumentation_dependencies(self) -> Collection[str]:
         return _instruments
@@ -252,10 +298,10 @@ class PeeweeInstrumentor(BaseInstrumentor):
 
         meter = _get_meter(meter_provider)
 
-        active_connections = meter.create_up_down_counter(
-            name="db.client.connection.count",
+        connection_usage = meter.create_up_down_counter(
+            name=MetricInstruments.DB_CLIENT_CONNECTIONS_USAGE,
             unit="connection",
-            description="The number of active connections",
+            description="The number of connections that are currently in state described by the state attribute.",
         )
         duration_histogram = meter.create_histogram(
             name="db.client.operation.duration",
@@ -266,12 +312,13 @@ class PeeweeInstrumentor(BaseInstrumentor):
 
         enable_commenter = kwargs.get('enable_commenter', False)
         commenter_options = kwargs.get('commenter_options', {})
+        enable_attribute_commenter = kwargs.get('enable_attribute_commenter', False)
 
-        peewee.Database.connect = _wrap_connect(tracer, active_connections)
+        peewee.Database.connect = _wrap_connect(tracer, connection_usage)
         peewee.Database.execute_sql = _wrap_execute_sql(tracer=tracer, duration_histogram=duration_histogram,
                                                         enable_sqlcommenter=enable_commenter,
-                                                        commenter_options=commenter_options)
-        peewee.Database.close = _wrap_close(active_connections)
+                                                        commenter_options=commenter_options, enable_attribute_commenter=enable_attribute_commenter)
+        peewee.Database.close = _wrap_close(connection_usage)
 
     def _uninstrument(self, **kwargs):
         peewee.Database.connect = self.original_connect
